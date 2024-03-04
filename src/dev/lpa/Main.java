693ea27a8bcca3a18fb1f2884811f5f4b35a1971
adamd <adam.d.hinton@gmail.com>
2024-03-03 19:31:09 -0500

    flesh out my first Interface

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,17 +5,79 @@ public class Main {
 
         Bird bird = new Bird();
 
+        Animal animal = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
+
+        animal.move();
+        // These don't work, compiler only cares about declared type
+//        flier.move();
+//        tracked.move();
+//        flier.fly();
+//        flier.takeOff();
+//        tracked.track();
+//        flier.land();
+
+        inFlight(flier);
     }
+
+    private static void inFlight (FlightEnabled flier){
+        flier.takeOff();
+        flier.fly();
+        if(flier instanceof Trackable tracked){
+            tracked.track();
+        }
+        flier.land();
+    }
+
 }
 
+// We don't have to declare an interface or its methods `abstract`
+// `abstract` is already implicitly implied
+// In fact, any mtd without a body is really implicitly both public and abstract
 interface FlightEnabled{
+    void takeOff();
+    void land();
+    void fly();
 
 }
 
-interface Trackable{}
+interface Trackable{
 
-abstract class Animal{};
+    void track();
+}
+
+abstract class Animal{
+
+    public abstract void move();
+
+};
 
 class Bird extends Animal implements FlightEnabled, Trackable{
 
+    @Override
+    public void move() {
+        System.out.println("Flaps wings");
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + " taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + " landing");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " flying");
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "'s coordinated recorded");
+    }
 }
