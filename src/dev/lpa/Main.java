b7272fdf7de6ae125b0045e5c5ab5c46f3118414
adamd <adam.d.hinton@gmail.com>
2024-03-03 19:59:21 -0500

    enums and records can implement interfaces

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -44,6 +44,62 @@ public class Main {
 
 }
 
+enum FlightStages implements Trackable{
+    GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if(this != GROUNDED){
+            System.out.println("Monitoring " + this );
+        }
+    }
+}
+
+record Dragonfly(String name, String type) implements FlightEnabled{
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Satellite implements OrbitEarth{
+    @Override
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved!");
+    }
+
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled{
+    void achieveOrbit ();
+
+}
+
 // We don't have to declare an interface or its methods `abstract`
 // `abstract` is already implicitly implied
 // In fact, any mtd without a body is really implicitly both public and abstract
